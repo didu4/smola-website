9c0d4818f6d4a434140a850f6dc3fffdf972b2b8
didu4 <solosar42@gmail.com>
2026-09-07 04:21:59 +0400

    style: отредачил верстку на мейн странице

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,54 +5,52 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        {/* Левый блок */}
-        <div className="logo-block">
-          <h3>СМОЛА</h3>
-          <p>Современная архитектурная школа</p>
-        </div>
+        <div className="footer-main">
+          <div className="logo-block">
+            <h3>СМОЛА</h3>
+            <p>Современная архитектурная школа</p>
+          </div>
 
-        {/* Навигация */}
-        <div className="nav-block">
-          <h4>Навигация</h4>
-          <ul>
-            <li>
-              <a href="#">О школе</a>
-            </li>
-            <li>
-              <a href="#">Программы</a>
-            </li>
-            <li>
-              <a href="#">Учителя</a>
-            </li>
-            <li>
-              <a href="#">Отзывы</a>
-            </li>
-            <li>
-              <a href="#">Цены</a>
-            </li>
-          </ul>
-        </div>
+          <div className="nav-block">
+            <h4>Навигация</h4>
+            <ul>
+              <li>
+                <a href="#">О школе</a>
+              </li>
+              <li>
+                <a href="#">Программы</a>
+              </li>
+              <li>
+                <a href="#">Учителя</a>
+              </li>
+              <li>
+                <a href="#">Отзывы</a>
+              </li>
+              <li>
+                <a href="#">Цены</a>
+              </li>
+            </ul>
+          </div>
 
-        {/* Контакты */}
-        <div className="contacts-block">
-          <h4>Связь с нами</h4>
-          <ul>
-            <li>
-              <a href="mailto:smolarch@smolarch.ru">smolarch@smolarch.ru</a>
-            </li>
-            <li>+7 987 654 32 10</li>
-          </ul>
-          <div className="socials">
-            <a href="#" aria-label="ВКонтакте">
-              <SiVk />
-            </a>
-            <a href="#" aria-label="Telegram">
-              <SiTelegram />
-            </a>
+          <div className="contacts-block">
+            <h4>Связь с нами</h4>
+            <ul>
+              <li>
+                <a href="mailto:smolarch@smolarch.ru">smolarch@smolarch.ru</a>
+              </li>
+              <li>+7 987 654 32 10</li>
+            </ul>
+            <div className="socials">
+              <a href="#" aria-label="ВКонтакте">
+                <SiVk />
+              </a>
+              <a href="#" aria-label="Telegram">
+                <SiTelegram />
+              </a>
+            </div>
           </div>
         </div>
 
-        {/* Копирайт */}
         <div className="copyright">
           <p>© 2026 Все права защищены</p>
           <p>Политика конфиденциальности</p>
